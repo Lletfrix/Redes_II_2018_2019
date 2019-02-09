75f65b923d51994cd2ddb6b8b41677558bed5ff6
Rafael Sánchez Sánchez <rafael.sanchezs@estudiante.uam.es>
2019-02-09 16:56:02 +0100

    daemon compiles

--- includes/daemon.h
+++ includes/daemon.h
@@ -1,6 +1,14 @@
 #ifndef DAEMON_H
 #define DAEMON_H
 
-    int demonizar(char *service);
+
+/**
+* FUNCIÓN:     int demonizar(char *service)
+* ARGS_IN:     char *service - identificador del servicio.
+* DESCRIPCIÓN: pasa el proceso a modo demonio. Lo ejecuta en segundo plano.
+* ARGS_OUT:    int - devuelve 0 en ejecución correcta. Devuelve un entero
+*              negativo en caso de error.
+*/
+int demonizar(const char *service);
 
 #endif
