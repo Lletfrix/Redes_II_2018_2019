4d2a67d229c4c99fb32e413967ca92ea61569967
Sergio Galán Martín <sergio.galanm@estudiante.uam.es>
2019-02-27 13:56:55 +0100

    GET fully funcional

--- srclib/process.c
+++ srclib/process.c
@@ -5,6 +5,9 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
 
 #include "../includes/picohttpparser.h"
 #include "../includes/process.h"
@@ -21,13 +24,14 @@
 #define MAXEXT 10
 #define MAXTYPE 50
 #define MAXAUX 4096
-#define MAXRESPONSE 999999999
+#define MAXRESPONSE 999
+#define CHUNK 1024
 
 /* CONFIG FILE*/
-#define ABSDIR "/mnt/c/Users/Sergamar/Desktop/Uni/RedesII/www"
+#define ABSDIR "/home/alumnos/e356632/RedesII/www"
 #define SVRNAME "MyServer"
 
-int get_response(char* path, struct phr_header* headers, size_t num_headers, char** response);
+int get_handler(char* path, struct phr_header* headers, size_t num_headers, int clientfd);
 char* post_response(char* path, struct phr_header* headers, size_t num_headers);
 char* options_response(char* path, struct phr_header* headers, size_t num_headers);
 char* bad_request_response();
@@ -42,8 +46,8 @@ int process_http(int clientfd){
     int len, pret, minor_version;
     size_t prevbuflen = 0, method_len, path_len, num_headers;
     char buf[MAXBUF], method_aux[MAXMETH], path_aux[MAXPATH];
-    char* response;
-    int size;
+    //char* response;
+    //int size;
     struct phr_header headers[MAXHEADERS];
     len = recv(clientfd, buf, MAXBUF, 0);
     printf("%s\n", buf);
@@ -57,8 +61,7 @@ int process_http(int clientfd){
     sprintf(method_aux, "%.*s", (int)method_len, method);
     sprintf(path_aux, "%.*s", (int)path_len, path);
     /*sprintf(ext, "%s", strrchr(path_aux, '.'));*/
-    size = get_response(path_aux, headers, num_headers, &response);
-    printf("%s", response);
+    get_handler(path_aux, headers, num_headers, clientfd); //Testing
     /*if(!strcmp(method_aux, "GET")){
         res = get_response(path, headers, num_headers);
     }
@@ -82,8 +85,14 @@ int process_http(int clientfd){
               (int)headers[i].value_len, headers[i].value);
     }
     printf("%s\n", buf+pret);*/
-    send(clientfd, response, size, 0);
+    //send(clientfd, response, size, 0);
     return 0;
+}
+
+int get_size(char* path){
+    struct stat atrib;
+    stat(path, &atrib);
+    return atrib.st_size;
 }
 
 int gmt_time_http(char** res, int tam){
@@ -150,14 +159,18 @@ int get_type(char* ext, char** res){
     return 0;
 }
 
-int get_response(char* path, struct phr_header* headers, size_t num_headers, char** response){
-    FILE* fp;
+int get_handler(char* path, struct phr_header* headers, size_t num_headers, int clientfd){
+    //FILE* fp;
     int size;
     char abspath[MAXPATH], sizestr[MAXSIZE];
     char* ext;
-    char* content;
+    char* response;
+    //char* content;
     char* type;
     char* aux;
+    int filefd;
+    off_t offset = 0;
+    bzero(abspath, MAXPATH);
     strcat(abspath, ABSDIR);
     printf("\n%s\n", path);
     if(!strcmp(path, "/")){
@@ -168,31 +181,42 @@ int get_response(char* path, struct phr_header* headers, size_t num_headers, cha
     if(!get_type(ext, &type)){
         /*return not_implemented_response()*/
     }
-    fp = fopen(abspath, "rb");
+    /*fp = fopen(abspath, "rb");
     if(!fp){
-        /*return not_found_response()*/
+        return not_found_response()
         printf("No he abierto el fichero");
+    }*/
+    response = calloc(MAXRESPONSE, sizeof(char));//TODO
+    /*content = calloc(MAXFILE, sizeof(char));
+    size = fread(content, 1 ,MAXFILE, fp);*/
+
+    filefd = open(abspath, O_RDONLY);
+    if(filefd < 0){
+        //return not_found_response()
     }
-    *response = calloc(MAXRESPONSE, sizeof(char));//TODO
-    content = calloc(MAXFILE, sizeof(char));
-    size = fread(content, 1 ,MAXFILE, fp);
+    size = get_size(abspath);
     printf("%s\n", abspath);
-    strcat(*response, "HTTP/1.1 200 OK\r\n");
+    strcat(response, "HTTP/1.1 200 OK\r\n");
     /*HEADERS*/
     general_headers(&aux);
-    strcat(*response, aux);
-    strcat(*response, "Last-Modified: ");
+    strcat(response, aux);
+    strcat(response, "Last-Modified: ");
     last_modified_http(abspath, &aux, MAXAUX);
-    strcat(*response, aux);
-    strcat(*response, "\r\n");
+    strcat(response, aux);
+    strcat(response, "\r\n");
     sprintf(sizestr, "%d", size);
-    strcat(*response, "Content-Length: ");
-    strcat(*response, sizestr);
-    strcat(*response, "\r\n");
-    strcat(*response, "Content-Type: ");
-    strcat(*response, type);
-    strcat(*response, "\r\n\r\n");
+    strcat(response, "Content-Length: ");
+    strcat(response, sizestr);
+    strcat(response, "\r\n");
+    strcat(response, "Content-Type: ");
+    strcat(response, type);
+    strcat(response, "\r\n\r\n");
     /*BODY*/
-    strcat(*response, content);
-    return size;
+    //strcat(*response, content);
+    send(clientfd, response, strlen(response), 0);
+    while(offset < size){
+        printf("\n Enviando %s\n", abspath);
+        sendfile(clientfd, filefd, &offset, CHUNK);
+    }
+    return 1;
 }
--- srclib/test-http-request.c
+++ srclib/test-http-request.c
@@ -11,23 +11,29 @@
 #include "../includes/connections.h"
 #include "../includes/process.h"
 
+int sockfd, clientfd;
+void handle_sigint(int sig)
+{
+    close_connection(sockfd);
+    close_connection(clientfd);
+    exit(EXIT_SUCCESS);
+}
+
 int main(void){
     char ip[] = "127.0.0.1";
     int port = 5001;
-    int sockfd, clientfd;
     struct sockaddr_in client;
     socklen_t addrlen = sizeof(client);
 
     sockfd = tcp_listen(ip, port);
     printf("Estamos escuchando\n");
 
-    clientfd = accept_connection(sockfd, (struct sockaddr*)&client, &addrlen);
-    printf("Conexión recibida: %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+    signal(SIGINT, handle_sigint);
 
-    process_http(clientfd);
-
-    close_connection(clientfd);
-    close_connection(sockfd);
-
-
+    while(1){
+        clientfd = accept_connection(sockfd, (struct sockaddr*)&client, &addrlen);
+        printf("Conexión recibida: %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+        process_http(clientfd);
+        close_connection(clientfd);
+    }
 }
