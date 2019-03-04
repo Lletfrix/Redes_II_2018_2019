c31f0d07037a514e56e4bbc9e955879f920d7bce
Sergio Galán Martín <sergio.galanm@estudiante.uam.es>
2019-03-04 01:18:43 +0100

    Scripts partially implemented

--- srclib/process.c
+++ srclib/process.c
@@ -26,10 +26,14 @@
 #define MAXAUX 4096
 #define MAXRESPONSE 999
 #define CHUNK 4096
+#define MAXSCRIPT 10
+#define MAXCOMMAND 500
+#define MAXRESULT 1000
 
 /* CONFIG FILE*/
 #define ABSDIR "/mnt/c/Users/Sergamar/Desktop/Uni/RedesII/www"
 #define DIR404 "/mnt/c/Users/Sergamar/Desktop/Uni/RedesII/www/404.html"
+#define SCRTXT "/mnt/c/Users/Sergamar/Desktop/Uni/RedesII/scripts/aux.txt"
 #define SVRNAME "MyServer"
 
 int get_handler(char* path, struct phr_header* headers, size_t num_headers, int clientfd);
@@ -42,6 +46,8 @@ int gmt_time_http(char** res, int tam);
 int general_headers(char** res);
 int last_modified_http(char* path, char** res, int tam);
 int get_type(char* ext, char** res);
+int file_exists(char* abspath);
+int process_script(char* abspath, char* body, int clientfd);
 
 int process_http(int clientfd){
     const char *method, *path;
@@ -142,7 +148,7 @@ int get_type(char* ext, char** res){
 
 int get_handler(char* path, struct phr_header* headers, size_t num_headers, int clientfd){
     int size;
-    char abspath[MAXPATH], sizestr[MAXSIZE];
+    char abspath[MAXPATH], sizestr[MAXSIZE], script_ext[MAXSCRIPT];
     char* ext;
     char* response;
     char* type;
@@ -171,8 +177,21 @@ int get_handler(char* path, struct phr_header* headers, size_t num_headers, int 
     }
     strcat(abspath, path);
     ext = strrchr(path, '.');
-    if(!get_type(ext, &type)){
+    if(!get_type(ext, &type) && !strrchr(ext,'?')){
         not_found_response(clientfd);
+        free(aux);
+        free(response);
+        free(type);
+        return EXIT_SUCCESS;
+    }
+    if(strrchr(ext, '?')){
+        sprintf(script_ext, "%.*s", (int)(strrchr(ext, '?') - ext), ext);
+        if(!strcmp(script_ext, ".py") || !strcmp(script_ext, ".php")){
+            process_script(abspath, NULL, clientfd);
+        }
+        else{
+            not_found_response(clientfd);
+        }
         free(aux);
         free(response);
         free(type);
@@ -293,5 +312,61 @@ int bad_request_response(int clientfd){
     strcat(response, "\r\n");
     send(clientfd, response, strlen(response), 0);
     free(response);
+    return EXIT_SUCCESS;
+}
+
+int process_script(char* abspath, char* body, int clientfd){
+    char* ext;
+    char* param;
+    char command[MAXCOMMAND];
+    char result[MAXRESULT];
+    char script_ext[MAXSCRIPT];
+    char file_url[MAXPATH];
+    FILE* pipe;
+    FILE* fp;
+    ext = strrchr(abspath, '.');
+    param = strrchr(abspath, '?');
+    sprintf(file_url, "%.*s", (int)(param-abspath), abspath);
+    if(file_exists(file_url)){
+        return EXIT_FAILURE;
+    }
+    sprintf(script_ext, "%.*s", (int)(param-ext), ext);
+    if(body){
+        fp = fopen(SCRTXT, "w");
+        fwrite(body, sizeof(char), strlen(body), fp);
+        fclose(fp);
+    }
+    if(!strcmp(script_ext, ".py")){
+        sprintf(command, "python3 %s ", file_url);
+        strcat(command, param+1);
+        strcat(command, " < ");
+        if(body){
+            strcat(command, SCRTXT);
+        }
+        else{
+            strcat(command, "/dev/null");
+        }
+        pipe = popen(command, "r");
+        fread(result, sizeof(char), MAXRESULT, pipe);
+        pclose(pipe);
+    }
+    if(!strcmp(script_ext, ".php")){
+        sprintf(command, "php %.*s", (int)(param-abspath), abspath);
+        if(body){
+            strcat(command, " < ");
+            strcat(command, SCRTXT);
+        }
+        pipe = popen(command, "r");
+        fread(result, sizeof(char), MAXRESULT, pipe);
+        pclose(pipe);
+    }
+    return EXIT_SUCCESS;
+}
+
+
+int file_exists(char *name){
+    FILE *fp;
+    if(!(fp = fopen(name, "r"))) return EXIT_FAILURE;
+    fclose(fp);
     return EXIT_SUCCESS;
 }
