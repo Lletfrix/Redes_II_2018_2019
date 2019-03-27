40cc9a8929e66b8ea042a65ecc55ba43e39ebf22
Rafael Sánchez Sánchez <rafael.sanchezs@estudiante.uam.es>
2019-03-27 14:24:50 +0100

    Add method to encrypt the file only·

--- document_sign.py
+++ document_sign.py
@@ -18,13 +18,15 @@ class docusign:
                 self.content = data.read()
             self.ciphered = None
             self.session_key = get_random_bytes(32)
+            self.iv = get_random_bytes(16)
         else:
-            self.digital_envelope = bin[:256]
-            self.ciphered = bin[256:]
+            self.digital_envelope = bin[16:272]
+            self.ciphered = bin[272:]
             self.hash = SHA256.new()
             self.digital_sign = None
             self.content = None
             self.session_key = None
+            self.iv = bin[:16]
 
     def generate_hash(self):
         self.hash.update(self.content)
@@ -34,27 +36,28 @@ class docusign:
         self.digital_sign = pkcs1_15.new(private_key).sign(self.hash)
 
     def cipher(self, private_key):
-        iv = get_random_bytes(16)
-        cipher_aes = AES.new(self.session_key,AES.MODE_CBC,iv=iv)
+        cipher_aes = AES.new(self.session_key,AES.MODE_CBC,iv=self.iv)
         binary = self.digital_sign + self.content
         self.ciphered=cipher_aes.encrypt(pad(binary, 16))
-        self.ciphered = iv + self.ciphered
+
+    def encrypt(self, public_key):
+        cipher_aes = AES.new(self.session_key,AES.MODE_CBC,iv=self.iv)
+        self.ciphered=cipher_aes.encrypt(pad(self.content, 16))
 
     def get_digital_envelope(self, public_key):
         cipher_rsa = PKCS1_OAEP.new(public_key)
         self.digital_envelope = cipher_rsa.encrypt(self.session_key)
 
     def prepare_upload(self):
-        self.ciphered = self.digital_envelope + self.ciphered
+        self.ciphered = self.iv + self.digital_envelope + self.ciphered
 
     def get_session_key(self, private_key):
         cipher_rsa = PKCS1_OAEP.new(private_key)
         self.session_key = cipher_rsa.decrypt(self.digital_envelope)
 
     def decipher(self):
-        iv = self.ciphered[:16]
-        cipher_aes = AES.new(self.session_key, AES.MODE_CBC, iv=iv)
-        binary = unpad(cipher_aes.decrypt(self.ciphered[16:]), 16)
+        cipher_aes = AES.new(self.session_key, AES.MODE_CBC, iv=self.iv)
+        binary = unpad(cipher_aes.decrypt(self.ciphered), 16)
         self.digital_sign = binary[:256]
         self.content = binary[256:]
         self.generate_hash()
