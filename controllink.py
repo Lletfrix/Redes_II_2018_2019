af45b05de25b2b7d743caf6468e5a751b3a147e4
Rafael Sánchez Sánchez <rafael.sanchezs@estudiante.uam.es>
2019-04-27 19:04:10 +0200

    Updated control link interface to adapt to gui

--- controllink.py
+++ controllink.py
@@ -3,57 +3,67 @@ import threading
 
 class ControlLink:
     timeoutDelay = 60
-    def __init__(self, ownAddr):
+    def __init__(self):
         # TCP peer socket
-        peerSocket = None
+        self.peerSocket = None
         # TCP server socket
-        svSocket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-        svSocket.bind((ownAddr))
+        self.svSocket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
         # TCP server thread
-        svThr = threading.Thread(target=tcpSv, daemon=True, args=(self))
+        self.svThr = threading.Thread(target=self.tcpSv, daemon=True)
         # Connection info
-        udpInPort = udpInPort
-        usrCalled = None
-        udpOutPort = None
-        alive=False
+        self.ownNick = None
+        self.ownAddr = None
+        self.udpInPort = None
+        self.usrCalled = None
+        self.udpOutPort = None
+        self.alive=False
         self.busy = False
-        self.busymtx = Lock()
+        self.busymtx = threading.Lock()
 
-    def hold(self):
-        # TODO: Handle errors
-        self.peerSocket.send(b"CALL_HOLD" + self.usrCalled)
+    def setNick(self, nick):
+        self.ownNick = nick
 
-    def resume(self):
-        # TODO: Handle errors
-        self.peerSocket.send(b"CALL_RESUME " + self.usrCalled)
+    def setINET(self, addr, udpInPort):
+        self.ownAddr = addr
+        self.udpInPort = udpInPort
 
     def setDest(self, userCalled, udpOutPort):
         self.userCalled = userCalled
         self.udpOutPort = udpOutPort
 
-    def call(self, addr, userCalled, udpOutPort):
+    def hold(self):
+        # TODO: Handle errors
+        self.peerSocket.send(b"CALL_HOLD " + self.ownNick)
+
+    def resume(self):
+        # TODO: Handle errors
+        self.peerSocket.send(b"CALL_RESUME " + self.ownNick)
+
+    def call(self, addr):
         if self.getBusyState():
             return False
         self.toggleBusy()
         # TODO: Handle sck.timeout
-        self.setDest(userCalled, updOutPort)
         self.peerSocket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
         self.peerSocket.settimeout(timeout)
         self.peerSocket.connect(addr)
-        self.peerSocket.send(b"CALLING " + self.userCalled + b" " + self.udpInPort)
-        resp = self.peerSocket.recv()
+        self.peerSocket.send(b"CALLING " + self.ownNick + b" " + self.udpInPort)
+        resp = self.peerSocket.recv() # TODO: Handle invalid resp
         (cmd, nick, port) = resp.decode('ascii').split(' ')
         if cmd == "CALL_ACCEPTED":
             self.usrCalled = nick.encode('ascii')
-            self.port = port.encode('ascii')
+            self.udpOutPort = port.encode('ascii')
             return True
         self.peerSocket.close()
         self.toggleBusy()
         return False
 
-    def start(self):
+    def start(self, nick, addr, udpInPort):
+        self.setNick(nick)
+        self.setINET(addr, udpInPort)
         self.alive = True
-        svThr.start()
+        self.svSocket.bind(self.ownAddr)
+        self.svThr.start()
 
     def tcpSv(self):
         self.svSocket.listen(5)
@@ -69,16 +79,16 @@ class ControlLink:
 
     def answerCall(self, answer):
         if answer:
-            self.peerSocket.send(b"CALL_ACCEPTED " + self.usrCalled + b" " + self.udpInPort)
+            self.peerSocket.send(b"CALL_ACCEPTED " + self.ownNick + b" " + self.udpInPort)
         else:
-            self.peerSocket.send(b"CALL_DENIED " + nick)
+            self.peerSocket.send(b"CALL_DENIED " + self.ownNick)
             self.peerSocket.close()
             self.peerSocket = None
             self.toggleBusy()
 
     def hang(self):
-        self.peerSocket.send(b"CALL_END " + self.usrCalled)
-        self.peerSocket.shutdown(SHUT_RDWR)
+        self.peerSocket.send(b"CALL_END " + self.ownNick)
+        self.peerSocket.close()
         self.peerSocket = None
         self.toggleBusy()
 
